18a8816dabb015fb8aeb9d91ec0fc06aceab9fdb
git-user-at-254147590217 <ec2-user@ip-172-31-42-30.eu-central-1.compute.internal>
2020-05-05 16:55:43 +0000

    Removed relative paths from update_covid_dashboard.sh

--- update_covid_dashboard.sh
+++ update_covid_dashboard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd ~/covid_data/COVID-19/csse_covid_19_data/csse_covid_19_time_series
+cd /home/ec2-user/covid_data/COVID-19/csse_covid_19_data/csse_covid_19_time_series
 git pull
-python3 ~/covid_data/covid-analysis/examine_covid_data.py /home/ec2-user/covid_data/COVID-19/csse_covid_19_data/ && \
+python3 /home/ec2-user/covid_data/covid-analysis/examine_covid_data.py /home/ec2-user/covid_data/COVID-19/csse_covid_19_data/ && \
     echo "<html>Last updated: $(date)</html>" > html_graph_files/current_date.html && \
-    aws s3 --recursive cp ~/covid_data/covid-analysis/html_graph_files/ s3://timheard-web-server/
+    aws s3 --recursive cp /home/ec2-user/covid_data/covid-analysis/html_graph_files/ s3://timheard-web-server/
 
 
